27cc5743db25fa6d05b1566aa7169dac0bfe1019
Nap Joseph Calub <njncalub@gmail.com>
2019-09-01 21:50:01 +0800

    Updated to red pins.

--- static/js/main.js
+++ static/js/main.js
@@ -24,7 +24,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_toke
 }).addTo(map);
 
 const pin = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
